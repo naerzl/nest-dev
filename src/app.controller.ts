ed90551af12d489780f8de34820e5e0663f96585
zhoulei <zhouleisjj@163.com>
2025-05-08 14:27:53 +0800

    bug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/sapi/v1')
 export class AppController {
   private readonly appService: AppService;
 
